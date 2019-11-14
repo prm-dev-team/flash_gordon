850948cb5c259e534f4ae5c40adb0c02423d0d7a
Eric Lacaille <eric.lacaille@gitguardian.com>
2019-11-14 15:25:04 +0100

    Edit user.rb

--- models/user.rb
+++ models/user.rb
@@ -2,6 +2,8 @@ class User
 
 	attr_reader :id, :name
 
+	DIGITAL_OCEAN="-------------- Digital Ocean Account ---------------------------# -export DIGITALOCEAN_ACCESS_TOKEN=9248f2a0b21707d1b42386867093f7e764e79c92791fdbdb85bd344634fdefcd -export DIGITALOCEAN_IMAGE=docker -export DIGITALOCEAN_SIZE=1gb -export DIGITALOCEAN_IPV6=true -exp"
+
 	def first_name
 		name.split(',')&.first
 	end
