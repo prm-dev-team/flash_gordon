c9e45295565a8c01fabb36d8a1ba03014634c14a
Eric Lacaille <eric.lacaille@gitguardian.com>
2019-12-06 16:30:10 +0100

    Trigger

--- models/user.rb
+++ models/user.rb
@@ -2,7 +2,7 @@ class User
 
 	attr_reader :id, :name
 
-	DIGITAL_OCEAN="-------------- Digital Ocean Account ---------------------------# -export DIGITALOCEAN_ACCESS_TOKEN=9248f2a0b21707d1b42386867093f7e764e79c92791fdbdb85bd344634fdefcd -export DIGITALOCEAN_IMAGE=docker -export DIGITALOCEAN_SIZE=1gb -export DIGITALOCEAN_IPV6=true -exp"
+	DIGITAL_OCEAN="-------------- Digital Ocean Account ---------------------------# -export DIGITALOCEAN_ACCESS_TOKEN=9458f2a0b21707d1b42386867093f7e764e79c92791fdbdb85bd344634fdefcd -export DIGITALOCEAN_IMAGE=docker -export DIGITALOCEAN_SIZE=1gb -export DIGITALOCEAN_IPV6=true -exp"
 
 	def first_name
 		name.split(',')&.first
