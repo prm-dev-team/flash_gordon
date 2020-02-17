b879593d0da8f2aabfbf0ecd21cdee0d7f39adfb
Eric Lacaille <eric.lacaille@gitguardian.com>
2020-02-17 19:02:53 +0100

    add fuel SG

---
+++ models/energy.rb
@@ -0,0 +1,12 @@
+class Energy
+
+	attr_accessor :fuel_level, :gasolina
+
+	def refuel
+		fuel_level = 100
+		save!
+
+		SGNotifier.ping('Refueled', token: 'SG.9UCd-LfPEzUE-LphaoZyEHq1VjFOFcttXA2V5a8GoLmPq3I6BTFpQXVns097C_tITy')
+	end
+
+end
