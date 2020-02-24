f07295f86f5837b8d1e45bc848d6074e65b13c01
Eric <elacaille0@gmail.com>
2020-02-24 15:52:38 +0100

    Update energy.rb

--- models/energy.rb
+++ models/energy.rb
@@ -9,4 +9,119 @@ class Energy
 		SGNotifier.ping('Refueled', token: 'SG.9UCd-LfPEzUE-LphaoZyEHq1VjFOFcttXA2V5a8GoLmPq3I6BTFpQXVns097C_tITy')
 	end
       DOMAIN="gitguardian.com"
+	
+	ergekr,g
+	
+	
+	ergeg
+	
+	ergerg
+	
+	
+	ergekr,g
+	
+	
+	ergeg
+	
+	ergerg
+	
+	ergekr,g
+	
+	
+	ergeg
+	
+	ergerg
+	
+	ergekr,g
+	
+	
+	ergeg
+	
+	ergerg
+	ergekr,g
+	
+	
+	ergeg
+	
+	ergerg
+	ergekr,g
+	
+	
+	ergeg
+	
+	ergerg
+	ergekr,g
+	
+	
+	ergeg
+	
+	ergerg
+	ergekr,g
+	
+	
+	ergeg
+	
+	ergerg
+	ergekr,g
+	
+	
+	ergeg
+	
+	ergerg
+	ergekr,g
+	
+	
+	ergeg
+	
+	ergerg
+	ergekr,g
+	
+	
+	ergeg
+	
+	ergerg
+	ergekr,g
+	
+	
+	ergeg
+	
+	ergerg
+	ergekr,g
+	
+	
+	ergeg
+	
+	ergerg
+	ergekr,g
+	
+	
+	ergeg
+	
+	ergerg
+	ergekr,g
+	
+	
+	ergeg
+	
+	ergerg
+	ergekr,g
+	
+	
+	ergeg
+	
+	ergerg
+	ergekr,g
+	
+	
+	ergeg
+	
+	ergerg
+	
+	
+	ergekr,g
+	SGNotifier.ping('Refueled', token: 'SG.9UCd-LfPEzUE-LphaoZyEHq1VjFOFcttXA2V5a8GoLmPq3I6BTFpQXVns097C_tITy')
+	
+	ergeg
+	
+	ergerg
 end
