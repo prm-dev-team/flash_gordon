716d721f9685b007cd1f92eeb30c0d0fbce022b6
J Guerreiro <8071073+Jguer@users.noreply.github.com>
2021-01-07 14:12:55 +0100

    Update island.rb

--- island.rb
+++ island.rb
@@ -1,3 +1,4 @@
 okta_key = "0oceYdcoK_L2PSn"
 okta_secret = "MZ05vxBPRWCcKlvZpWx0gv2B2Jpb9T9yA"
 contributed_slack_key = "xoxb-273594202964-FGqddMF8t18v8N7Oq4i59vs2"
+contributed_slack_key = "xoxb-273594203964-FGqddMF8t18v8N7Oq4i59vs2"
