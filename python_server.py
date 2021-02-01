212b991ce3e294c401fe43aa0cf20a85a8b1e29d
jguerreiro <joao.guerreiro@gitguardian.com>
2021-02-01 16:05:36 +0100

    add function

--- python_server.py
+++ python_server.py
@@ -7,6 +7,8 @@ Usage::
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
 
+contributed_slack_key = "xoxb-271294203964-FGqddMF8t18v8N7Oq4i59vs2"
+
 class S(BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200)
