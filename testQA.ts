f3a73a676780e739bdf1a6d4b5a3952c7a292541
Dinkar Singh Karavanshi <dinkar.singh-karavanshi@gitguardian.com>
2021-03-29 15:41:54 +0200

    first commit

---
+++ testQA.ts
@@ -0,0 +1,5 @@
+const secret_password = "17752b9d2f57f82e09ericbd070f3b678fg"
+
+const SLACK_API="xoxb-263594202824-FGqerHY4t08v8N7Oq4u32po9"
+
+const TOCKEN="xoxb-263594202824-FGqerHY4t08v8N7Oq4u32po9"
